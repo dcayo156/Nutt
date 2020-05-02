7b38b40772b0846d9e79f09fd90722c12fc429e9
dcayo156 <dcayo.156@gmail.com>
2020-05-01 20:02:22 -0400

    Pathology functionality complete

--- SGC.LIB/NEG/LO/GPatologias.cs
+++ SGC.LIB/NEG/LO/GPatologias.cs
@@ -2,6 +2,10 @@ using System;
 using System.Data;
 using XP.AUT;
 using NUT.LIB.NEG.EN;
+using NUT.LIB.NEG.AD;
+using NUT.LIB.NEG.SF;
+using XP.LO;
+using System.Transactions;
 
 namespace NUT.LIB.NEG.LO
 {
@@ -17,7 +21,10 @@ namespace NUT.LIB.NEG.LO
 
         /********** VARIOS REGISTROS ***********/
 
-
+        public static DataSet BuscarPatologias(FPatologias filter)
+        {
+            return DPatologias.BuscarPatologias(filter);
+        }
 
         /************** REPORTES ***************/
 
@@ -26,15 +33,57 @@ namespace NUT.LIB.NEG.LO
         #endregion
 
         #region Verificaciones
-
-
+        public static bool ExisteNombreUsuario(string nombre, int idPatologia)
+        {
+            return DPatologias.ExisteNombrePatologia(nombre, idPatologia);
+        }
+        public static bool ExistePatologiaEnHistoriaClinica(int idPatologia)
+        {
+            return DPatologias.ExistePatologiaEnHistoriaClinica(idPatologia);
+        }
 
         #endregion
 
         #region Transacciones
 
-
-
+        public static void RegistrarPatologia(Patologia eUsuario)
+        {
+            //Actualizar la entidad Patologia falta el campo Nombre
+            if (ExisteNombreUsuario("Nombre" , eUsuario.Id.Value))
+            {
+                throw new LogicaException("La nombre especificado ya pertenece a otra Patologia", "NombreExistente");
+            }
+            using (TransactionScope ts = new TransactionScope())
+            {
+                Insertar(eUsuario);
+                ts.Complete();
+            }
+        }
+        public static void ModificarPatologia(Patologia ePatologia)
+        {
+            //Actualizar la entidad Patologia falta el campo Nombre
+            if (ExisteNombreUsuario("Nombre", ePatologia.Id.Value))
+            {
+                throw new LogicaException("La nombre especificado ya pertenece a otra Patologia", "NombreExistente");
+            }
+            using (TransactionScope ts = new TransactionScope())
+            {
+                Modificar(ePatologia);
+                ts.Complete();
+            }
+        }
+        public static void EliminarPatologia(int idPatologia)
+        {
+            if (ExistePatologiaEnHistoriaClinica(idPatologia))
+            {
+                throw new LogicaException("La patología esta asociada a una historia clinica", "PatologiaRelacionada");
+            }   
+            using (TransactionScope ts = new TransactionScope())
+            {
+                DPatologias.EliminarPatologia(idPatologia);
+                ts.Complete();
+            }
+        }
         #endregion
 
         #region Procesos
---
+++ SGC.LIB/NEG/SF/FPatologias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUT.LIB.NEG.SF
+{
+    public class FPatologias
+    {
+        public string Nombre { get; set; }
+        public string EsHabitoFisiologico { get; set; }
+        public int Estado { get; set; }
+    }
+}

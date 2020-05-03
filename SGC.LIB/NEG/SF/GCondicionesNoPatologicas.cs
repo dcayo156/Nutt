83790685cd182b61b99a2e80e853ab8885298bf4
dcayo156 <dcayo.156@gmail.com>
2020-05-03 12:52:53 -0400

    Add Filter Condicion No Patologica

---
+++ SGC.LIB/NEG/SF/GCondicionesNoPatologicas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUT.LIB.NEG.SF
+{
+  public  class GCondicionesNoPatologicas
+    {
+        public string nombre { get; set; }
+        public int estado   { get; set; }
+        public string CargarEnHistoriaClinica { get; set; }
+    }
+}

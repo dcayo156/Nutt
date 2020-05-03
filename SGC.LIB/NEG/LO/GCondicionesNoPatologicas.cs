e23bcec8e931f175d4434f6daf9370b2a0b4a267
dcayo156 <dcayo.156@gmail.com>
2020-05-03 13:02:54 -0400

    Changed class logic Condicion no patologica

--- SGC.LIB/NEG/LO/GCondicionesNoPatologicas.cs
+++ SGC.LIB/NEG/LO/GCondicionesNoPatologicas.cs
@@ -2,6 +2,10 @@ using System;
 using System.Data;
 using XP.AUT;
 using NUT.LIB.NEG.EN;
+using NUT.LIB.NEG.AD;
+using NUT.LIB.NEG.SF;
+using XP.LO;
+using System.Transactions;
 
 namespace NUT.LIB.NEG.LO
 {
@@ -13,11 +17,17 @@ namespace NUT.LIB.NEG.LO
 
         /******** VALOR/REGISTRO ÚNICO *********/
 
-
+        public static DataSet TraerCondicionNoPatologica(int idCondicionNoPatologica)
+        {
+            return DCondicionesNoPatologicas.TraerCondicionNoPatologica(idCondicionNoPatologica);
+        }
 
         /********** VARIOS REGISTROS ***********/
 
-
+        public static DataSet BuscarCondicionNoPatologicas(FCondicionesNoPatologicas filter)
+        {
+            return DCondicionesNoPatologicas.BuscarCondicionNoPatologicas(filter);
+        }
 
         /************** REPORTES ***************/
 
@@ -26,15 +36,63 @@ namespace NUT.LIB.NEG.LO
         #endregion
 
         #region Verificaciones
-
-
+        public static bool ExisteNombreCondicionNoPatologica(string nombre, int idCondicionNoPatologica)
+        {
+            return DCondicionesNoPatologicas.ExisteNombreCondicionNoPatologica(nombre, idCondicionNoPatologica);
+        }
+        public static bool ExisteCondicionNoPatologicaEnHistoriaClinica(int idCondicionNoPatologica)
+        {
+            return DCondicionesNoPatologicas.ExisteCondicionNoPatologicaEnHistoriaClinica(idCondicionNoPatologica);
+        }
 
         #endregion
 
         #region Transacciones
 
-
-
+        public static void RegistrarCondicionNoPatologica(CondicionNoPatologica eCondicionNoPatologica)
+        {
+            DateTime ahora = DateTime.Now;
+            if (ExisteNombreCondicionNoPatologica(eCondicionNoPatologica.Nombre, eCondicionNoPatologica.Id.Value))
+            {
+                throw new LogicaException("El nombre especificado ya pertenece a otra Condición no Patologica", "NombreExistente");
+            }
+            eCondicionNoPatologica.IdUsuReg = usr.Id;
+            eCondicionNoPatologica.FecReg = ahora;
+            eCondicionNoPatologica.IdUsuMod = usr.Id;
+            eCondicionNoPatologica.FecMod = ahora;
+            using (TransactionScope ts = new TransactionScope())
+            {
+                Insertar(eCondicionNoPatologica);
+                ts.Complete();
+            }
+        }
+        public static void ModificarCondicionNoPatologica(CondicionNoPatologica eCondicionNoPatologica)
+        {
+            DateTime ahora = DateTime.Now;
+            if (ExisteNombreCondicionNoPatologica(eCondicionNoPatologica.Nombre, eCondicionNoPatologica.Id.Value))
+            {
+                throw new LogicaException("El nombre especificado ya pertenece a otra Condición no Patologica", "NombreExistente");
+            }
+            eCondicionNoPatologica.IdUsuMod = usr.Id;
+            eCondicionNoPatologica.FecMod = ahora;
+            using (TransactionScope ts = new TransactionScope())
+            {
+                Modificar(eCondicionNoPatologica, "IdUsuReg", "FecReg");
+                ts.Complete();
+            }
+        }
+        public static void EliminarCondicionNoPatologica(int idCondicionNoPatologica)
+        {
+            if (ExisteCondicionNoPatologicaEnHistoriaClinica(idCondicionNoPatologica))
+            {
+                throw new LogicaException("La condición no patologica esta asociada a una historia clinica", "CondicionNoPatologicaRelacionada");
+            }
+            using (TransactionScope ts = new TransactionScope())
+            {
+                Eliminar(new CondicionNoPatologica { Id = idCondicionNoPatologica });
+                ts.Complete();
+            }
+        }
         #endregion
 
         #region Procesos
@@ -42,7 +100,6 @@ namespace NUT.LIB.NEG.LO
 
 
         #endregion
-
     }
 
 }  
